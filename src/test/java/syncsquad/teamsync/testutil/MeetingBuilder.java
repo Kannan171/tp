e2eb6a39a5bae877436324bf19cda93a8f247e97
lilyium <150597285+lilyium@users.noreply.github.com>
2025-03-25 21:57:25 +0800

    Add MeetingBuilder class to help with building Meeting objects
    Make date and time fields private in Meeting class
    Add getters to Meeting class

---
+++ src/test/java/syncsquad/teamsync/testutil/MeetingBuilder.java
@@ -0,0 +1,71 @@
+package syncsquad.teamsync.testutil;
+
+import static syncsquad.teamsync.logic.parser.ParserUtil.DATE_FORMATTER;
+import static syncsquad.teamsync.logic.parser.ParserUtil.TIME_FORMATTER;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import syncsquad.teamsync.model.meeting.Meeting;
+
+/**
+ * A utility class to help with building Meeting objects.
+ */
+public class MeetingBuilder {
+
+    public static final String DEFAULT_DATE = "31-03-2025";
+    public static final String DEFAULT_STARTTIME = "14:00";
+    public static final String DEFAULT_ENDTIME = "16:00";
+
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    /**
+     * Creates a {@code MeetingBuilder} with the default details.
+     */
+    public MeetingBuilder() {
+        date = LocalDate.parse(DEFAULT_DATE, DATE_FORMATTER);
+        startTime = LocalTime.parse(DEFAULT_STARTTIME, TIME_FORMATTER);
+        endTime = LocalTime.parse(DEFAULT_ENDTIME, TIME_FORMATTER);
+    }
+
+    /**
+     * Initializes the MeetingBuilder with the data of {@code personToCopy}.
+     */
+    public MeetingBuilder(Meeting meetingToCopy) {
+        date = meetingToCopy.getDate();
+        startTime = meetingToCopy.getStartTime();
+        endTime = meetingToCopy.getEndTime();
+    }
+
+
+    /**
+     * Sets the {@code date} of the {@code Meeting} that we are building.
+     */
+    public MeetingBuilder withDate(String date) {
+        this.date = LocalDate.parse(date, DATE_FORMATTER);
+        return this;
+    }
+
+    /**
+     * Sets the {@code startTime} of the {@code Meeting} that we are building.
+     */
+    public MeetingBuilder withStartTime(String startTime) {
+        this.startTime = LocalTime.parse(startTime, TIME_FORMATTER);
+        return this;
+    }
+
+    /**
+     * Sets the {@code endTime} of the {@code Meeting} that we are building.
+     */
+    public MeetingBuilder withEndTime(String endTime) {
+        this.endTime = LocalTime.parse(endTime, TIME_FORMATTER);
+        return this;
+    }
+
+    public Meeting build() {
+        return new Meeting(date, startTime, endTime);
+    }
+
+}
