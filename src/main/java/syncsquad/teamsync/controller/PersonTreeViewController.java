199ef16140e9012a8e10ad1d62d1754e8857b08c
kerktaiheng <taihengkerk@gmail.com>
2025-03-30 13:42:41 +0800

    Remove calls to getTreeItem from within updateItem

--- src/main/java/syncsquad/teamsync/controller/PersonTreeViewController.java
+++ src/main/java/syncsquad/teamsync/controller/PersonTreeViewController.java
@@ -23,14 +23,65 @@ import syncsquad.teamsync.viewmodel.PersonListViewModel;
 public class PersonTreeViewController extends UiPart<Region> {
     private static final String FXML = "PersonTreeView.fxml";
 
+    private abstract static class PersonCard {
+
+        private Person person;
+
+        public PersonCard(Person person) {
+            this.person = person;
+        }
+
+        public Person getPerson() {
+            return person;
+        }
+
+    }
+
+    private static class PersonDummyRootCard extends PersonCard {
+
+        public PersonDummyRootCard(Person person) {
+            super(person);
+        }
+    }
+
+    private static class PersonRootCard extends PersonCard {
+
+        private int index;
+
+        public PersonRootCard(Person person, int index) {
+            super(person);
+            this.index = index;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    private static class PersonInfoCard extends PersonCard {
+
+        public PersonInfoCard(Person person) {
+            super(person);
+        }
+    }
+
+    private static class PersonModulesCard extends PersonCard {
+
+        public PersonModulesCard(Person person) {
+            super(person);
+        }
+    }
+
     //TreeView requires a root node to be set, so we create a dummy root node
     private static final Person ROOT_DUMMY_PERSON = new Person(
         new Name("People"), new Phone("777"), new Email("root@root.com"),
         new Address("dummy"), new HashSet<>(), new HashSet<>());
-    private static final TreeItem<Person> ROOT_TREE_ITEM = new TreeItem<>(ROOT_DUMMY_PERSON);
+    private static final TreeItem<PersonCard> ROOT_TREE_ITEM =
+            new TreeItem<>(new PersonDummyRootCard(ROOT_DUMMY_PERSON));
+
 
     @FXML
-    private TreeView<Person> personTreeView;
+    private TreeView<PersonCard> personTreeView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
@@ -42,21 +93,26 @@ public class PersonTreeViewController extends UiPart<Region> {
         ROOT_TREE_ITEM.setExpanded(true);
 
         viewModel.personListProperty().forEach(person -> {
-            TreeItem<Person> personItem = new TreeItem<>(person);
-            personItem.getChildren().add(new TreeItem<>(person)); // Contact Info
-            personItem.getChildren().add(new TreeItem<>(person)); // Modules
-            ROOT_TREE_ITEM.getChildren().add(personItem);
-            personItem.setExpanded(true);
+            TreeItem<PersonCard> personRootItem = new TreeItem<>(
+                    new PersonRootCard(person, viewModel.personListProperty().indexOf(person) + 1));
+            TreeItem<PersonCard> personInfoItem = new TreeItem<>(new PersonInfoCard(person));
+            TreeItem<PersonCard> personModulesItem = new TreeItem<>(new PersonModulesCard(person));
+            personRootItem.getChildren().add(personInfoItem);
+            personRootItem.getChildren().add(personModulesItem);
+            ROOT_TREE_ITEM.getChildren().add(personRootItem);
+            personRootItem.setExpanded(true);
         });
 
         viewModel.personListProperty().addListener((ListChangeListener<Person>) change -> {
             ROOT_TREE_ITEM.getChildren().clear();
             viewModel.personListProperty().forEach(person -> {
-                TreeItem<Person> personItem = new TreeItem<>(person);
-                personItem.getChildren().add(new TreeItem<>(person)); // Contact Info
-                personItem.getChildren().add(new TreeItem<>(person)); // Modules
-                ROOT_TREE_ITEM.getChildren().add(personItem);
-                personItem.setExpanded(true);
+                TreeItem<PersonCard> personRootItem = new TreeItem<>(
+                        new PersonRootCard(person, viewModel.personListProperty().indexOf(person) + 1));
+                TreeItem<PersonCard> personInfoItem = new TreeItem<>(new PersonInfoCard(person));
+                TreeItem<PersonCard> personModulesItem = new TreeItem<>(new PersonModulesCard(person));
+                personRootItem.getChildren().add(personInfoItem);
+                personRootItem.getChildren().add(personModulesItem);
+                ROOT_TREE_ITEM.getChildren().add(personRootItem);
             });
         });
 
@@ -67,37 +123,24 @@ public class PersonTreeViewController extends UiPart<Region> {
      * Custom {@code TreeCell} implementation for displaying {@code Person} objects.
      * DO NOT USE ATLANTAFX STYLES HERE. Learnt that the hard way.
      */
-    private static class PersonTreeCell extends TreeCell<Person> {
+    private static class PersonTreeCell extends TreeCell<PersonCard> {
         @Override
-        protected void updateItem(Person person, boolean empty) {
-            //lines 74 to 78 must not be modified
-            super.updateItem(person, empty);
-            if (empty || person == null) {
+        protected void updateItem(PersonCard personCard, boolean empty) {
+            //lines 129 to 133 must not be modified
+            super.updateItem(personCard, empty);
+            if (empty || personCard == null) {
                 setText(null);
                 setGraphic(null);
-                return;
-            }
-
-            if (getTreeItem() == null) {
-                setText(null);
-                setGraphic(null);
-                return;
-            }
-
-            if (person == ROOT_DUMMY_PERSON) {
-                setGraphic(new PersonDummyRootCardController().getRoot());
             } else {
-                setText(null);
-                if (!getTreeItem().isLeaf()) {
-                    setGraphic(new PersonRootCardController(person,
-                            getTreeItem().getParent().getChildren().stream()
-                                    .map(TreeItem::getValue).toList().indexOf(person) + 1).getRoot());
-                } else {
-                    if (getTreeItem().getParent().getChildren().indexOf(getTreeItem()) == 0) {
-                        setGraphic(new PersonInfoCardController(person).getRoot());
-                    } else {
-                        setGraphic(new PersonModulesCardController(person).getRoot());
-                    }
+                if (personCard instanceof PersonDummyRootCard) {
+                    setGraphic(new PersonDummyRootCardController().getRoot());
+                } else if (personCard instanceof PersonRootCard) {
+                    setGraphic(new PersonRootCardController(
+                            personCard.getPerson(), ((PersonRootCard) personCard).getIndex()).getRoot());
+                } else if (personCard instanceof PersonInfoCard) {
+                    setGraphic(new PersonInfoCardController(personCard.getPerson()).getRoot());
+                } else if (personCard instanceof PersonModulesCard) {
+                    setGraphic(new PersonModulesCardController(personCard.getPerson()).getRoot());
                 }
             }
         }
