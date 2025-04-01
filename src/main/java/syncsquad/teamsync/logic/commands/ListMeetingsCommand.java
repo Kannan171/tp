1c327b793a35677b637c5d08ff221464019e8914
lilyium <150597285+lilyium@users.noreply.github.com>
2025-04-02 00:38:04 +0800

    Add equals and toString method to ListMeetingsCommand
    ListMeetingsCommand does not override the equals and toString method
    
    This is inconsistent with the design of all other Commands
    
    Let's override the equals and toString method

--- src/main/java/syncsquad/teamsync/logic/commands/ListMeetingsCommand.java
+++ src/main/java/syncsquad/teamsync/logic/commands/ListMeetingsCommand.java
@@ -2,6 +2,7 @@ package syncsquad.teamsync.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import syncsquad.teamsync.commons.util.ToStringBuilder;
 import syncsquad.teamsync.model.Model;
 
 /**
@@ -18,5 +19,21 @@ public class ListMeetingsCommand extends Command {
         requireNonNull(model);
         String meetingsDisplay = String.join("\n", MESSAGE_SUCCESS, model.displayMeetingsString());
         return new CommandResult(meetingsDisplay);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // returns true if other is a ListMeetingsCommand object
+        return other instanceof ListMeetingsCommand;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).toString();
     }
 }
