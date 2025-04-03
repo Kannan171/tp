f72b53ffda4a5d62e2bc8048aec26f557557ac07
Lee Wai Kin <lwk19@comp.nus.edu.sg>
2025-04-03 18:47:26 +0800

    Fix various alpha bugs

--- src/main/java/syncsquad/teamsync/logic/commands/person/AddPersonCommand.java
+++ src/main/java/syncsquad/teamsync/logic/commands/person/AddPersonCommand.java
@@ -38,7 +38,8 @@ public class AddPersonCommand extends PersonCommand {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book. \n"
+            + "Ensure that the email address is different from an existing one. ";
 
     private final Person toAdd;
 
